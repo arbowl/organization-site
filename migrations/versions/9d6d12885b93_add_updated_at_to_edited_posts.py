7070b89598129c8c4d20d1325d464efded17ddb6
Drew B <103056414+arbowl@users.noreply.github.com>
2025-07-13 11:24:16 -0400

    Fix migration hallucination

--- migrations/versions/9d6d12885b93_add_updated_at_to_edited_posts.py
+++ migrations/versions/9d6d12885b93_add_updated_at_to_edited_posts.py
@@ -1,7 +1,7 @@
 """Add 'updated_at' column to posts table
 
-Revision ID: a1b2c3d4e5f6
-Revises: 9d6d12885b93  # or your actual last revision ID
+Revision ID: 9d6d12885b93
+Revises: 1233ab03841b  # or your actual last revision ID
 Create Date: 2025-07-13 14:00:00.000000
 
 """
@@ -9,8 +9,8 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = 'a1b2c3d4e5f6'
-down_revision = '9d6d12885b93'
+revision = '9d6d12885b93'
+down_revision = '1233ab03841b'
 branch_labels = None
 depends_on = None
 
