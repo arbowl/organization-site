6b67b2d7f5919c5a22e4f6be1d12cb38f8dd3540
Drew <103056414+arbowl@users.noreply.github.com>
2025-10-20 10:47:17 -0400

    Add host name to emails

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ basedir = path.abspath(path.dirname(__file__))
 class Config:
     SECRET_KEY = environ.get("SECRET_KEY") or "12345"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SERVER_NAME = environ.get("HOST_DOMAIN", "localhost")
+    PREFERRED_URL_SCHEME = environ.get("PREFERRED_URL_SCHEME", "https")
 
 
 class DevelopmentConfig(Config):
@@ -13,6 +15,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = environ.get(
         "DEV_DATABASE_URL"
     ) or "sqlite:///" + path.join(basedir, "instance", "app.db")
+    PREFERRED_URL_SCHEME = environ.get("PREFERRED_URL_SCHEME", "http")
 
 
 class TestingConfig(Config):
